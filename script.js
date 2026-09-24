59606a2e0822301576c524f66b7fcc981e1ee422
Estalin34 <estalinleon06@hotmail.com>
2026-09-23 21:09:16 -0500

    Mejoras

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let activado = false;
 boton.addEventListener("click", () => {
 
     sorpresa.textContent =
-        "Que nunca te falten motivos para sonreír 💛🌻";
+        "Que nunca te falten motivos para sonreír Juliana  💛🌻";
 
     boton.innerHTML =
         "💛 Estas flores son para ti 🌻";
@@ -21,7 +21,7 @@ boton.addEventListener("click", () => {
     if (!activado) {
 
         escribirMensaje(
-            "Porque te mereces cosas bonitas y muchos motivos para sonreír ✨"
+            "Porque te mereces cosas bonitas y muchos motivos para sonreír Juliana  ✨"
         );
 
         activado = true;
